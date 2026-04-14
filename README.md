41bbb6bd92a86842dc8fcb679524fbd21d112ae6
Rowan Victor <209355544+rowanvictor01@users.noreply.github.com>
2026-04-14 22:43:19 +0800

    Setup xmake project config. Fix README typo

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Car Racing Game
 
 A simple 2D racing game where the player races with an A.I. car.
-The game is writtin in C++ and and built with the SFML library.
+The game is writtin in C++ and built with the SFML library.
--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,15 @@
-add_rules("mode.debug", "mode.release")
+add_requires("sfml")
+set_toolchains("gcc-15")
+set_languages("cxx23")
+add_cxxflags("-pedantic-errors", "-Wall", "-Wextra", "-Wconversion", "-Wsign-conversion", "-Weffc++", "-Werror", "-g")
+
 
 target("car-racing-game")
     set_kind("binary")
     add_files("src/*.cpp")
+    add_includedirs("include/")
+    add_packages("sfml")
+
 
 --
 -- If you want to known more usage about xmake, please see https://xmake.io
